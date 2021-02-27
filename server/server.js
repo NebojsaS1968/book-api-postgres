dd4dd95c8d60e39f60e000b82205fbe2beca1bfb
NebojsaS1968 <savicnebojsa10@gmail.com>
2021-02-27 19:09:43 +0100

    installing cors

--- server/server.js
+++ server/server.js
@@ -1,11 +1,13 @@
 const express = require("express");
 require("dotenv").config();
 const morgan = require("morgan");
+const cors = require("cors");
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 const app = express();
 
 // Middlewares
+app.use(cors());
 app.use(morgan("dev"));
 app.use(express.json());
 
