ea5fddba6b848a510b154df8aa88ac3756320898
NebojsaS1968 <savicnebojsa10@gmail.com>
2021-03-01 11:51:06 +0100

    setting up book details page

--- client/src/routes/BookDetails.jsx
+++ client/src/routes/BookDetails.jsx
@@ -1,6 +1,9 @@
-import React from "react";
+import React, { useEffect, useContext } from "react";
+import { BooksContext } from "../context/BooksContext";
 
 const BookDetails = () => {
+  const { books, setBooks } = useContext(BooksContext);
+
   return <div>Book Details</div>;
 };
 
