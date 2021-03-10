ca464972fceb75a78903e609e0d80f54b16c63ce
NebojsaS1968 <savicnebojsa10@gmail.com>
2021-03-10 14:37:34 +0100

    setting new state for changing themes and toggle function

--- client/src/context/BooksContext.js
+++ client/src/context/BooksContext.js
@@ -5,14 +5,28 @@ export const BooksContext = createContext();
 export const BooksContextProvider = (props) => {
   const [books, setBooks] = useState([]);
   const [selectedBook, setSelectedBook] = useState([]);
+  const [isLight, setIsLight] = useState(true);
 
   const addBook = (book) => {
     setBooks([...books, book]);
   };
 
+  const toggle = () => {
+    setIsLight(!isLight);
+  };
+
   return (
     <BooksContext.Provider
-      value={{ books, setBooks, addBook, selectedBook, setSelectedBook }}
+      value={{
+        books,
+        setBooks,
+        addBook,
+        selectedBook,
+        setSelectedBook,
+        isLight,
+        setIsLight,
+        toggle,
+      }}
     >
       {props.children}
     </BooksContext.Provider>
